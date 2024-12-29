961fd42ae95ee16f9f39c8b70d67617ba385455a
misdarmanto <misdarmanto1922@gmail.com.com>
2024-12-29 16:42:47 +0700

    fix ui

--- resources/seeders/2_productSeeder.js
+++ resources/seeders/2_productSeeder.js
@@ -7,10 +7,11 @@ module.exports = {
       {
         product_id: 1,
         product_user_id: 1,
-        product_name: 'Product A',
-        product_description: 'A high-quality product for everyday use.',
+        product_name: 'Kura Kura rawa',
+        product_description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         product_category_id: 1, // Ensure this corresponds to an existing category ID
-        product_price: 29.99,
+        product_price: 1200000,
         product_weight: 1.5,
         product_colors: 'Red, Blue',
         product_sizes: 'M, L, XL',
@@ -22,10 +23,11 @@ module.exports = {
       {
         product_id: 2,
         product_user_id: 2,
-        product_name: 'Product B',
-        product_description: 'A luxury item designed for special occasions.',
+        product_name: 'Bulus',
+        product_description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         product_category_id: 2, // Ensure this corresponds to an existing category ID
-        product_price: 199.99,
+        product_price: 320000,
         product_weight: 2.0,
         product_colors: 'Gold, Silver',
         product_sizes: 'S, M, L',
@@ -37,10 +39,43 @@ module.exports = {
       {
         product_id: 3,
         product_user_id: 3,
-        product_name: 'Product C',
-        product_description: 'A practical product for professionals.',
+        product_name: 'Kura Kura Gunung',
+        product_description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         product_category_id: 3, // Ensure this corresponds to an existing category ID
-        product_price: 49.99,
+        product_price: 200000,
+        product_weight: 1.0,
+        product_colors: 'Black, White',
+        product_sizes: 'S, M',
+        product_transaction_type: 'Barter',
+        created_at: new Date(),
+        updated_at: new Date(),
+        deleted_at: null
+      },
+      {
+        product_id: 4,
+        product_user_id: 3,
+        product_name: 'Kura Kura Sawah',
+        product_description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        product_category_id: 3, // Ensure this corresponds to an existing category ID
+        product_price: 2300000,
+        product_weight: 1.0,
+        product_colors: 'Black, White',
+        product_sizes: 'S, M',
+        product_transaction_type: 'Barter',
+        created_at: new Date(),
+        updated_at: new Date(),
+        deleted_at: null
+      },
+      {
+        product_id: 5,
+        product_user_id: 3,
+        product_name: 'Kura Kura Desert',
+        product_description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        product_category_id: 3, // Ensure this corresponds to an existing category ID
+        product_price: 2300000,
         product_weight: 1.0,
         product_colors: 'Black, White',
         product_sizes: 'S, M',
--- resources/seeders/3_productImageSeerde.js
+++ resources/seeders/3_productImageSeerde.js
@@ -7,7 +7,8 @@ module.exports = {
       {
         product_image_id: 1,
         product_image_product_id: 1, // Assuming the product_id of the product is 1
-        product_image_url: 'https://example.com/product1-image1.jpg',
+        product_image_url:
+          'https://lh3.googleusercontent.com/proxy/HRMNV0vYu3jRnW1kp64hstf_fwzlPB4oMIwfsHHzvPKZ0M_EPOLBBu0PkGE1_KvTxNMigodTmboLvYWJaEnf3c6MRpYEJgAvb5gcgmbJhN7AtiLnG4ZHF-2_qXGZ-T1XSUEkS_F_YAZEUYrsKbn8KvUb9w5KWVjD2ika_DVHNiWRp0ZLdlctyInXrZT5poW8_LmYLJz5ubNCvw0',
         created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
@@ -15,15 +16,37 @@ module.exports = {
       {
         product_image_id: 2,
         product_image_product_id: 1, // Assuming the product_id of the product is 1
-        product_image_url: 'https://example.com/product1-image2.jpg',
+        product_image_url:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMvSrC-NLhLN0XexE9w2iKcLsAOVZi6tVaUukMrsRlgPpZhzXDWG3sHFqCB7Vdril5DX8&usqp=CAU',
+        created_at: new Date(),
+        updated_at: new Date(),
+        deleted_at: null
+      },
+
+      {
+        product_image_id: 3,
+        product_image_product_id: 2, // Assuming the product_id of the product is 2
+        product_image_url:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT208z2A58m7K5bE5RWpHNeZO67T55QO4rRDQ&s',
         created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
-        product_image_id: 3,
-        product_image_product_id: 2, // Assuming the product_id of the product is 2
-        product_image_url: 'https://example.com/product2-image1.jpg',
+        product_image_id: 4,
+        product_image_product_id: 3, // Assuming the product_id of the product is 2
+        product_image_url:
+          'https://img-z.okeinfo.net/library/images/2018/06/21/3ufji260tefvb4b16mvw_11872.jpg',
+        created_at: new Date(),
+        updated_at: new Date(),
+        deleted_at: null
+      },
+
+      {
+        product_image_id: 5,
+        product_image_product_id: 3, // Assuming the product_id of the product is 2
+        product_image_url:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT208z2A58m7K5bE5RWpHNeZO67T55QO4rRDQ&s',
         created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
